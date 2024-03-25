dc8829956319c88707dce599a3cc52eb14bc633a
Jarvi$ <jarvis@Jarvis-MacBook-Air.local>
2024-03-25 19:12:39 +0530

    Added navbar UI

--- components/MobileMenu.tsx
+++ components/MobileMenu.tsx
@@ -2,17 +2,27 @@ import React, { FC } from "react";
 
 interface MobileMenuProps {
   visible?: boolean;
-  label: string;
 }
-const MobileMenu: FC<MobileMenuProps> = ({ visible, label }) => {
-  if (visible) {
+const MobileMenu: FC<MobileMenuProps> = ({ visible }) => {
+  if (!visible) {
     return null;
   }
   return (
     <div className="bg-black w-56 absolute top-8 left-0 py-5 flex-col border-2 border-gray-800 flex">
       <div className="flex flex-col gap-4">
+        <div className="px-3 text-center text-white hover:underline">Home</div>
         <div className="px-3 text-center text-white hover:underline">
-          {label}
+          Series
+        </div>
+        <div className="px-3 text-center text-white hover:underline">Films</div>
+        <div className="px-3 text-center text-white hover:underline">
+          New & Popular
+        </div>
+        <div className="px-3 text-center text-white hover:underline">
+          My List
+        </div>
+        <div className="px-3 text-center text-white hover:underline">
+          Browse by languages
         </div>
       </div>
     </div>
